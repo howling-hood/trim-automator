47350f91783de8d32ff0672d25e0426b63199210
officialk <karthik@officialk.dev>
2025-03-25 00:34:37 +0530

    initial setup

---
+++ app/main.js
@@ -0,0 +1,44 @@
+import path from "node:path";
+
+import { app, BrowserWindow } from "electron";
+
+import isDev from "electron-is-dev";
+
+const prodEnv = Boolean(process.env.NODE_ENV === "production");
+const isProd = !isDev || prodEnv;
+
+const createWindow = () => {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    title: "Trim Automator",
+    width: 800,
+    height: 600,
+    backgroundColor: "#312450",
+    webPreferences: {
+      devTools: true,
+      nodeIntegration: true
+    }
+  });
+
+  if (isProd || app.isPackaged) {
+    mainWindow.loadFile("ui/index.html");
+    mainWindow.webContents.openDevTools();
+    return;
+  }
+  mainWindow.webContents.openDevTools();
+  mainWindow.loadURL("http://localhost:3030");
+  return;
+};
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+
+export default defineConfig([
+  {
+    ignores: ["**/build/", "**/dist/", "**/coverage/", "**/node_modules/"]
+  },
+  {
+    files: ["./src/*.{js,mjs,cjs,jsx}", "./public/*.{js,mjs,cjs,jsx}"],
+    languageOptions: { globals: { ...globals.browser } }
+  },
+  {
+    files: ["./app/*.{js,mjs,cjs,jsx}", "./common/*.{js,mjs,cjs,jsx}"],
+    languageOptions: { globals: { ...globals.node } }
+  },
+  pluginReact.configs.flat.recommended,
+  eslintPluginPrettierRecommended
+]);
