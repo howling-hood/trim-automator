b860748e22ad1bdfb43de8a248b488ba9984db31
Karthik Koppaka <karthik@officialk.dev>
2025-03-25 03:27:05 +0530

    adding in basic navigational UI (#1)

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import Header from "./components/Header";
+import Gather from "./pages/Gather";
+import Reduce from "./pages/Reduce";
+import Cutter from "./pages/Cutter";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img
-          src={logo}
-          className="App-logo"
-          alt="logo"
+    <Router basename="/">
+      <Header />
+      <Routes>
+        <Route
+          path="/"
+          exact
+          Component={Gather}
         />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+        <Route
+          path="/reduce"
+          exact
+          Component={Reduce}
+        />
+        <Route
+          path="/cutter"
+          exact
+          Component={Cutter}
+        />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/Reduce/index.js
@@ -0,0 +1,13 @@
+import { Typography } from "@mui/material";
+import React from "react";
+
+const Reduce = () => {
+  return (
+    <Typography
+      variant="h4"
+      color="primary">
+      Reduce
+    </Typography>
+  );
+};
+export default Reduce;
