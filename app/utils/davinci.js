4249445eac1566df41f1b74cc41e83502c83acfa
Karthik Koppaka <karthik@officialk.dev>
2025-04-02 01:42:52 +0530

    Adding simulating functionality (#7)
    * updating shorcuts simulation
    
    * removing logs

--- app/utils/davinci.js
+++ app/utils/davinci.js
@@ -1,7 +1,24 @@
-export const gotoTimecode = (timecode, shortcut) => {
-  return `goto ${timecode}`;
+import { Key, keyboard } from "@nut-tree-fork/nut-js";
+import { keyMapping } from "./const.js";
+
+keyboard.config.autoDelayMs = 200;
+
+const getMappedShortcut = (shortcut) =>
+  shortcut.split("+").map((k) => {
+    let ky = k.toUpperCase();
+    return Key[ky] || keyMapping[ky] || ky;
+  });
+
+export const switchToDavinci = async () => {
+  await keyboard.type(Key.LeftAlt, Key.Tab);
 };
 
-export const performBladeCut = (shortcut) => {
-  return `goto ${shortcut}`;
+export const gotoTimecode = async (timecode, shortcut) => {
+  await keyboard.type(...getMappedShortcut(shortcut));
+  await keyboard.type(timecode.toString());
+  await keyboard.type(Key.Enter);
+};
+
+export const performBladeCut = async (shortcut) => {
+  await keyboard.type(...getMappedShortcut(shortcut));
 };
